47e667637f8a19037df144dbf384d334ba5b4fdd
Krishna <krishnagopalmandal3@gmail.com>
2026-06-14 23:20:25 +0530

    Configure frontend for production

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8001",
+  baseURL: "https://ai-legal-system-k3kz.onrender.com",
 });
 
 export const predictIPC = async (text) => {
